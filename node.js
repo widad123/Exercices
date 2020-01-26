14805a2eb6573224f1db745814a0d019d39d2dec
widad123 <khatiri.widad@gmail.com>
2020-01-26 23:13:45 +0100

    fin

--- node.js
+++ node.js
@@ -1,29 +1,4 @@
-/**
- * Exercice 1.2.5 : Explorer un arbre (JavaScript)
-Dans le langage de programmation de votre choix : 
 
-Ecrire la méthode qui permet d'ajouter un nœud dans un arbre binaire de recherche
-
-Définir la méthode pour trouver une valeur donnée dans un arbre binaire de recherche
-
-Écrire la méthode pour afficher l’arbre selon un parcours infixe
-
-[optionnel] Écrire la méthode pour supprimer un nœud donné en distinguant trois cas :
-
-Le nœud est une feuille -> suppression simple
-
-Le nœud a un seul enfant -> il est remplacé par lui
-
-Le nœud à deux enfants, on le remplace alors par le nœud le plus proche, c’est à dire le nœud le plus à droite de l’arbre gauche ou le plus à gauche de l'arbre droit.
-
- Un arbre est constitué de noeuds 
-Un arbre binaire est un arbre de degrés 2 sa taille (=racine+taille(filsGauche)+taille(filsDroite))
-chaque noeud ds un arbre binaire a que 2 enfants .. un arbre binaire se remplie de gauche vers la droite ..la valeur de gauche est supp à celle de droite
-
-une feuille est un noeud qui n'a pas d'enfants 
-Pour un noeud donné, ses enfants à gauches sont inférieurs et ses enfants à droite sont supérieurs
-gauche<racine<droite
-*/ 
 
 class Noeud{
     constructor(valeur){
@@ -122,9 +97,7 @@ trouverNoeud(valeur){
     }
 };
 //la méthode pour afficher l’arbre selon un parcours infixe
-/**
- * Pour afficher un arbre selon un parcours infixe je commence par les noeuds toutes a gauche jusqu'au noeuds toute à droite
- */
+
 afficherArbre(){
     if(this.racine!==undefined){
        return this.racine.afficherNoeud();
@@ -133,9 +106,7 @@ afficherArbre(){
     }
     
 };
-/*Écrire la méthode pour supprimer un nœud donné en distinguant trois cas :
-Le nœud est une feuille -> suppression simple
-Le nœud a un seul enfant -> il est remplacé par lui*/
+
 supprimerNoeud(){
 
 };
